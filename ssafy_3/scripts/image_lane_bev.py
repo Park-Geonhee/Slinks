d7ffc8431f1a0fcb2536781ab01995a67fd803ea
skymoon0228 <skymoon0228@naver.com>
2023-09-05 09:23:21 +0900

    Feat: Add BEV func

--- ssafy_3/scripts/image_lane_bev.py
+++ ssafy_3/scripts/image_lane_bev.py
@@ -10,51 +10,28 @@ import json
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridgeError
 
-# image_lane_bev 은 perspective view 형태의 이미지를 BEV(bird-eye-view) 형태로 전환하는 예제입니다.
-# IPM (Inverse Perspective Mapping)
-# Warping
-
-# 노드 실행 순서 
-# 1. 이미지 warping 영역 지정
-# 2. 원본 이미지 내 Warping 영역과 매칭 될 목적지 지점 정의
-# 3. 원근 맵 행렬 생성 함수를 이용하여 매핑 좌표에 대한 원근 맵 행렬을 생성
-# 4. 원근 맵 행렬에 대한 기하학적 변환
-
 def warp_image(img, source_prop):
     
     image_size = (img.shape[1], img.shape[0])
 
     x = img.shape[1]
     y = img.shape[0]
-    destination_points = np.float32([[0,480][280,200],[380,200][640,480]])
-    source_points = source_prop * np.float32([[x, y]]* 4)
+    l_t = [280,240]
+    l_b = [0,350]
+    r_t = [360,240]
+    r_b = [640,350]
+    ## left top->letf bottom->right top -> right bottom
+    source_points = np.float32([l_t,l_b,r_t,r_b])
+    destination_points = np.float32([[0,0],[0,480],[640,0],[640,480]])
 
-    #TODO: (2) 원본 이미지 내 Warping 영역과 매칭 될 목적지 지점 정의
-    '''
-    destination_points = np.float32()
-    
-    source_points = source_prop * np.float32([[x, y]]* 4)
-    '''
-    
-    # 이미지를 기하학적 변환을 해야합니다. 이미지를 인위적으로 확대, 축소, 위치 변경, 회전,
-    # 왜곡을 하는 것으로 BEV는 원근이 있는 이미지를 원근을 없애는 작업을 해야합니다.
-    # 기하학적 변환은 아핀변환과(Affine transformation) 원근변환(Perspective transformation)이 있습니다.
-    # 우리는 원근 변환이라는 OpenCV를 사용합니다.
-
-    #TODO: (3) 원근 맵 행렬 생성 함수를 이용하여 매핑 좌표에 대한 원근 맵 행렬을 생성합니다.
-    '''
-    perspective_transform = cv2.
-
-    '''
-    
-    #TODO: (4) 이후 원근 맵 행렬에 대한 기하학적 변환을 진행합니다.
-    '''
-    warped_img = cv2.
-
-    '''
     perspective_transform = cv2.getPerspectiveTransform(source_points,destination_points)
-    warped_img = cv2.warpPerspective(img, perspective_transform)
+    warped_img = cv2.warpPerspective(img, perspective_transform,image_size)
 
+    ## draw move points 
+    cv2.circle(img, tuple(l_t), 5, (255,0,0),-1)
+    cv2.circle(img, tuple(l_b), 5, (0,255,0),-1)
+    cv2.circle(img, tuple(r_t), 5, (0,0,255),-1)
+    cv2.circle(img, tuple(r_b), 5, (0,0,0),-1)
     return warped_img
 
 
@@ -64,12 +41,7 @@ class IMGParser:
         self.image_sub = rospy.Subscriber("/image_jpeg/compressed", CompressedImage, self.callback)
         self.img_bgr = None
 
-        #TODO: (1) 이미지 warping 영역 지정
-        '''
-        Bird's eye view 를 하기 위한 영역을 지정해야 합니다. 이지미 warping을 위해 영역을 비율로 만들어줘야 합니다.
-        self.source_prop = np.float32(
-        '''
-        self.source_prop = np.float32([])
+        self.source_prop = np.float32([0.7])
 
     def callback(self, msg):
         try:
@@ -78,9 +50,7 @@ class IMGParser:
         except CvBridgeError as e:
             print(e)
 
-        #TODO: (2-0) 이미지 Warping 시작
         img_warp = warp_image(self.img_bgr, self.source_prop)
-
         img_concat = np.concatenate([self.img_bgr, img_warp], axis=1)
 
         cv2.imshow("Image window", img_concat)
@@ -88,13 +58,9 @@ class IMGParser:
 
 
 def main():
-
     rospy.init_node('lane_birdview', anonymous=True)
-
     image_parser = IMGParser()
-
     rospy.spin()
 
 if __name__ == '__main__':
-    
     main()
