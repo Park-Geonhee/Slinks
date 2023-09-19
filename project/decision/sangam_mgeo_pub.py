8c47d57b95b3dbfd40b7a5f620b07bf18acbf20f
sku379829 <sku379829@gmail.com>
2023-09-19 13:26:29 +0900

    Feat : codes and launch file for Sangam map & set lane_change_penalty

--- project/decision/sangam_mgeo_pub.py
+++ project/decision/sangam_mgeo_pub.py
@@ -45,7 +45,7 @@ class get_mgeo :
         # MGeo에 정의되어 있는 데이터를 활용해 각 Node 와 Link 간 연결 성을 나타낼 수 있습니다.
         
         '''
-        load_path = os.path.normpath(os.path.join(current_path, 'lib/mgeo_data/R_KR_PG_K-City'))
+        load_path = os.path.normpath(os.path.join(current_path, 'lib/mgeo_data/R_KR_PR_Sangam_NoBuildings'))
         mgeo_planner_map = MGeo.create_instance_from_json(load_path)
 
         node_set = mgeo_planner_map.node_set
