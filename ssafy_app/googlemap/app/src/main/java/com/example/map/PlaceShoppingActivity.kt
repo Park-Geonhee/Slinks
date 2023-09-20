c2c9ba73f77bfc2e9382a2c00131edd34a4bd1cc
유지나 <wlskb@naver.com>
2023-09-20 17:59:27 +0900

    [+] add map page/googlemap으로 프로젝트  통합

--- ssafy_app/googlemap/app/src/main/java/com/example/map/PlaceShoppingActivity.kt
+++ ssafy_app/googlemap/app/src/main/java/com/example/map/PlaceShoppingActivity.kt
@@ -3,15 +3,9 @@ package com.example.map
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-
-class MainActivity : AppCompatActivity() {
-
-
+class PlaceShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_place_shopping)
     }
 }
-
-
--- ssafy_app/googlemap/app/src/main/java/com/example/map/PlanListActivity.kt
+++ ssafy_app/googlemap/app/src/main/java/com/example/map/PlanListActivity.kt
@@ -3,15 +3,9 @@ package com.example.map
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-
-class MainActivity : AppCompatActivity() {
-
-
+class PlanListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_plan_list)
     }
 }
-
-
