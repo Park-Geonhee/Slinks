b74768964006dcb549dc61a3cd8d27cb6670f183
유지나 <wlskb@naver.com>
2023-10-04 14:30:57 +0900

    [+] 페이지 이동 오류 없이 작동

---
+++ ssafy_app/googlemap/app/src/main/java/com/example/map/Diary.kt
@@ -0,0 +1,11 @@
+package com.example.map
+
+import File
+
+data class Diary(
+    val id: Long? = null,
+    val content: String? = null,
+    val user: User? = null,
+    val place: Place? = null,
+    val file: File? = null
+)
