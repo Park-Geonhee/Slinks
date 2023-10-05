b85350dafaf68c69f4e32aaf029b5fd866b9aae0
유지나 <wlskb@naver.com>
2023-10-06 03:07:25 +0900

    [+] change upload url

--- ssafy_backend/neplan/src/main/java/com/nemo/neplan/config/WebMvcConfig.java
+++ ssafy_backend/neplan/src/main/java/com/nemo/neplan/config/WebMvcConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Value("${upload.directory}")
     private String uploadDirectory="/usr/local/lib/upload-dir/";
 
     @Override
